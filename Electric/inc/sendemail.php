652482c5298894bb4dba30ba5552e40c870e972c
David Adeyemi <83539218+Davidadeyemi@users.noreply.github.com>
2023-11-13 12:46:31 +0000

    Update sendemail.php
    added new email info

--- Electric/inc/sendemail.php
+++ Electric/inc/sendemail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $dropdown = $POST['dropdown'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "your@gmail.com";
+$recipient = "info@kindomenginerring.co.uk";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
